959edfa38957f2e1247e0f2ddc7f43ad3c3a4d7f
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-07-09 18:56:05 -0700

    updated loadedMesh

--- src/engine/Assets/LoadedMesh.cpp
+++ src/engine/Assets/LoadedMesh.cpp
@@ -39,11 +39,11 @@ LoadedMesh Engine::Assets::loadStlMesh( std::string filename ) {
 
     if( ( normal - calcNorm ).lengthSquared() < 0.001f ) {
       triangles.push_back( { { v1.x, v1.y, v1.x, 0, 0 },
-                             { v2.x, v2.y, v2.z, 0.5, 0 },
+                             { v2.x, v2.y, v2.z, 1, 0 },
                              { v3.x, v3.y, v3.x, 1, 1 } } );
     } else {
       triangles.push_back( { { v3.x, v3.y, v3.x, 1, 1 },
-                             { v2.x, v2.y, v2.z, 0.5, 0 },
+                             { v2.x, v2.y, v2.z, 1, 0 },
                              { v1.x, v1.y, v1.x, 0, 0 } } );
     }
 
