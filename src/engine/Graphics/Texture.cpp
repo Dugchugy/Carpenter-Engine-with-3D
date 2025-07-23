eb5dbb71831c922baf5e5df649fa5be9acbb71fc
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2025-07-22 18:26:03 -0700

    added debug printing dimensions

--- src/engine/Graphics/Texture.cpp
+++ src/engine/Graphics/Texture.cpp
@@ -20,6 +20,8 @@ void Engine::Graphics::Texture::LoadTexture() {
   if ( textureData == nullptr ) {
     LoadTextureArray();
   }
+
+  std::cout << "dimensions (" << m_dimensions[1] << ", " << m_dimensions[0] << ")\n"
   
   std::cout << (short) textureData[0] << " " << (short) textureData[1] << " " <<
                (short) textureData[2] << " " << (short) textureData[3] << "\n";
